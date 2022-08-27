eddebc91c26cf06d1505cedd0a0710e1dd1d0b2a
ahmad <ahmed.b.charrada@gmail.com>
2022-08-27 17:15:24 +0100

    Add dynamodb db client mock

--- dynamodb/service.go
+++ dynamodb/service.go
@@ -4,18 +4,18 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
 )
 
-type DBClient dynamodbiface.DynamoDBAPI
+type DBClient interface{ dynamodbiface.DynamoDBAPI }
 
 // DynamodbWrapper provides the API methods for interacting with dynamo db
 type dynamodbWrapper[T EntityMarshaler[T]] struct {
-	conf DBConfig
-	DBClient
+	conf   DBConfig
+	client DBClient
 }
 
 // NewDynamoWrapper creates a new dynamo wrapper.
 func NewDynamoWrapper[T EntityMarshaler[T]](client DBClient, config DBConfig) *dynamodbWrapper[T] {
 	return &dynamodbWrapper[T]{
-		conf:     config,
-		DBClient: client,
+		conf:   config,
+		client: client,
 	}
 }

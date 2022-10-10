22312a07004c51ae35329d779ec9e056b1cffa81
ahmad <ahmed.b.charrada@gmail.com>
2022-10-10 09:22:08 +0100

    Add get by IDs implementation.

--- dynamodb/queries.go
+++ dynamodb/queries.go
@@ -1,8 +1,15 @@
 package dynamodb
 
 import (
+	"awsgo/utils"
 	"context"
 )
+
+type resp[T any] struct {
+	data            []T
+	unprocessedKeys []DynamoPrimaryKey
+	err             error
+}
 
 // Get implements Queries
 func (d *dynamodbWrapper[T]) Get(ctx context.Context, primaryKey DynamoPrimaryKey) (*T, error) {
@@ -37,6 +44,69 @@ func (d *dynamodbWrapper[T]) Get(ctx context.Context, primaryKey DynamoPrimaryKe
 }
 
 // GetByIDs implements Queries
-func (*dynamodbWrapper[T]) GetByIDs(context.Context, []DynamoPrimaryKey) ([]T, error) {
-	panic("unimplemented")
+func (d *dynamodbWrapper[T]) GetByIDs(ctx context.Context, ids []DynamoPrimaryKey) ([]T, error) {
+	partitions := utils.Partition(ids, 25)
+
+	ch := make(chan resp[T])
+	defer close(ch)
+
+	for part := range partitions {
+		go d.load(ctx, ch, part...)
+	}
+
+	res := make([]T, 0, len(ids))
+	for out := range ch {
+		if out.err != nil {
+			return nil, out.err
+		}
+
+		res = append(res, out.data...)
+	}
+
+	return res, nil
+}
+
+func (d *dynamodbWrapper[T]) load(ctx context.Context, ch chan<- resp[T], ids ...DynamoPrimaryKey) {
+	// build the batch get item query
+	query, err := NewExpressionBuilder(d.conf.TableInfo.TableName).BuildBatchGetItemInput(ids...)
+	if err != nil {
+		ch <- resp[T]{
+			err: err,
+		}
+		return
+	}
+
+	// trigger the query
+	out, err := d.client.BatchGetItemWithContext(ctx, query)
+	if err != nil {
+		ch <- resp[T]{
+			err: err,
+		}
+		return
+	}
+
+	// parse response and accumulate returned items
+	data := make([]T, 0, len(out.Responses[d.conf.TableInfo.TableName]))
+	for _, item := range out.Responses[d.conf.TableInfo.TableName] {
+		entity, err := (*new(T)).UnMarshal(item)
+		if err != nil {
+			ch <- resp[T]{
+				err: err,
+			}
+			return
+		}
+
+		data = append(data, entity)
+	}
+
+	res := resp[T]{
+		data: data,
+	}
+
+	if out.UnprocessedKeys != nil && len(out.UnprocessedKeys[d.conf.TableInfo.TableName].Keys) > 0 {
+		// Todo convert unprocessed keys to DynamoPrimaryKey
+		res.unprocessedKeys = []DynamoPrimaryKey{}
+	}
+
+	ch <- res
 }

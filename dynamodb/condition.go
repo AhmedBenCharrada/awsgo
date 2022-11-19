c53d3cf49ee06e922abadd027f3162d0ac0dd09e
ahmad <ahmed.b.charrada@gmail.com>
2022-11-19 08:53:54 +0100

    Add condition builder.

---
+++ dynamodb/condition.go
@@ -0,0 +1,57 @@
+package dynamodb
+
+import "github.com/aws/aws-sdk-go/service/dynamodb/expression"
+
+type Operator int
+
+const (
+	// EQUAL the equality operator
+	EQUAL Operator = iota + 1
+	// LT less-than operator
+	LT
+	// LE less or equal operator
+	LE
+	// GT greater-than operator
+	GT
+	// GE greater or equal operator
+	GE
+)
+
+type ConditionBuilder struct {
+	builder expression.ConditionBuilder
+}
+
+func NewConditionBuilder() *ConditionBuilder {
+	return &ConditionBuilder{
+		expression.ConditionBuilder{},
+	}
+}
+
+// Or applies the OR condition for the dynamo attribute
+func (cb *ConditionBuilder) Or(attribName string, value interface{}, operator Operator) *ConditionBuilder {
+	cond := cb.builder.Or(create(attribName, value, operator))
+	cb.builder = cond
+	return cb
+}
+
+// And applies the AND condition for the dynamo attribute
+func (cb *ConditionBuilder) And(attribName string, value interface{}, operator Operator) *ConditionBuilder {
+	cond := cb.builder.And(create(attribName, value, operator))
+	cb.builder = cond
+	return cb
+}
+
+func create(attribName string, value interface{}, operator Operator) expression.ConditionBuilder {
+	switch operator {
+	case LT:
+		return expression.Name(attribName).LessThan(expression.Value(value))
+	case LE:
+		return expression.Name(attribName).LessThanEqual(expression.Value(value))
+	case GT:
+		return expression.Name(attribName).GreaterThan(expression.Value(value))
+	case GE:
+		return expression.Name(attribName).GreaterThanEqual(expression.Value(value))
+	default:
+		return expression.Name(attribName).Equal(expression.Value(value))
+	}
+}
---
+++ dynamodb/condition_test.go
@@ -0,0 +1,27 @@
+package dynamodb_test
+
+import (
+	"awsgo/dynamodb"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateConditionBuilder(t *testing.T) {
+	cb := dynamodb.NewConditionBuilder().
+		And("attrib1", "some-value", dynamodb.EQUAL).
+		And("attrib2", "val", dynamodb.GT).
+		And("attrib3", "val", dynamodb.GE).
+		And("attrib4", "val", dynamodb.LT).
+		And("attrib4", "val", dynamodb.LE)
+
+	assert.NotNil(t, cb)
+
+	cb2 := dynamodb.NewConditionBuilder().And("attrib1", "some-value", dynamodb.EQUAL).
+		Or("attrib2", "val", dynamodb.GT).
+		Or("attrib3", "val", dynamodb.GE).
+		Or("attrib4", "val", dynamodb.LT).
+		Or("attrib4", "val", dynamodb.LE)
+
+	assert.NotNil(t, cb2)
+}
